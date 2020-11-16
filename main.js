2cf99e82d11cc79db45d7ffafbc0d428c8fd19bd
JamesObry <59664773+JamesObry@users.noreply.github.com>
2020-11-16 19:58:51 +0200

    Third commit
    I added new js code, css styles

---
+++ main.js
@@ -0,0 +1,7 @@
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 400) {
+      $('.header_title').addClass('newClass');
+    } else {
+      $('.header_title').removeClass('newClass');
+    }
+  });
